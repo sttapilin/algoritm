e283e0682c6de6b30af987db532a8cb6f3d31496
Stanislav <sttapilin@gmail.com>
2024-10-23 20:49:27 +0300

    Add files via upload

---
+++ ArrSort.java
@@ -0,0 +1,98 @@
+import java.util.Arrays;
+
+public class ArrSort {
+      // пузырек
+      public static long bubbleSort (int [] arr) {
+         long start = System.currentTimeMillis();
+         int arraysort [] = arr.clone();
+          for (int i = 0; i < arraysort.length - 1; i++) {
+              for (int j = 1; j < arraysort.length - i; j++) {
+                  if (arraysort [j - 1] > arraysort [j]) {
+                      int tmp = arraysort [j];
+                      arraysort [j] = arraysort [j - 1];
+                      arraysort [j - 1] = tmp;
+                  }
+              }
+          }
+          long end = System.currentTimeMillis();
+        return end - start;
+      }
+
+      // че-то свое
+      public static long mySort (int [] arr) {
+          long start = System.currentTimeMillis();
+          int arraysort [] = arr.clone();
+          int min = arraysort [0];
+          for (int i = 0; i < arraysort.length - 1; i++) {
+              min = arraysort [i];
+                for (int j = i + 1; j < arraysort.length; j++) {
+                    if (arraysort [j] < min) {
+                        min = arraysort [j];
+                        arraysort [j] = arraysort[i];
+                        arraysort [i] = min;
+                    }
+
+                }
+
+          }
+
+          long end = System.currentTimeMillis();
+        return end - start;
+
+      }
+
+      // стандартный
+      public static long arrSort (int [] arr) {
+          long start = System.currentTimeMillis();
+          Arrays.sort(arr);
+          long end = System.currentTimeMillis();
+       return end - start;
+      }
+
+      //quicksort
+      public static void quickSort (int [] arr, int low, int high) {
+          //границы массива
+          if (low >= high)
+              return;
+
+          // опорный элемент медиана
+          int middle = arr[low + (high - low) / 2];
+          // деление на подмассивы, больше и меньше медианы
+          int i = low, j = high;
+
+          while (i <= j) {
+              while (arr[i] < middle) {
+                  i++;
+              }
+              while (arr[j] > middle) {
+                  j--;
+              }
+              if (i <= j) {//меняем местами
+                  int temp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = temp;
+                  i++;
+                  j--;
+              }
+          }
+
+
+          // вызов рекурсии для сортировки левой и правой части
+          if (low < j)
+              quickSort(arr, low, j);
+
+          if (high > i)
+              quickSort(arr, i, high);
+
+
+
+      }
+
+// для поверки сортировки можно вставить этот код в любой метод, кроме quick (рекурсия!)
+/*  for (int k = 0; k < arr.length; k++) {
+        if (k % 100 == 0) {
+            System.out.println(" ");
+        }
+        System.out.print(" " + arr[k]);
+    }*/
+}
---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int [] array = new int [100000];
+
+        // заполнение массива случайными числами до 1000
+        for (int i = 0; i < array.length; i++){
+           array [i] = rnd.nextInt(1000) +1;
+        }
+
+        // пузырек
+        ArrSort.bubbleSort(array);
+        System.out.println("Время сортировки алгоритма \"пузырек\": " + ArrSort.bubbleSort(array));
+        // че-то сам придумал
+        System.out.println("Время сортировки моего алгоритма: " + ArrSort.mySort(array));
+        // квик сорт
+
+        System.out.println("Время сортировки стандартной функции: " + ArrSort.arrSort(array));
+
+        long start = System.currentTimeMillis();
+        ArrSort.quickSort(array,0, array.length -1);
+        long end = System.currentTimeMillis();
+        System.out.println("Время сортировки QuickSort: " + (end - start));
+       /* System.out.println("");
+        int i =0;
+        for (int s : array) {
+
+            if (i%50 == 0) {
+                System.out.println("");
+            }
+            System.out.print(s +" ");
+            i++;
+        }*/
+
+
+    }
+
+}
